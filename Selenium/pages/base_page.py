520290a080b92f9082f9101039f114008b5f23fd
Lerato Moloi <moloil405@gmail.com>
2025-12-17 22:55:45 +0200

    Initializing first instance of POM

--- Selenium/pages/base_page.py
+++ Selenium/pages/base_page.py
@@ -2,4 +2,16 @@ from selenium.webdriver.support.ui import WebDriverWait # For explicit waits
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-def 
+class BASEPAGE():
+    # Base class for all page objects
+    # Seriously, learn Python properly on Sololearn or something.
+    Base_url= "https://automationteststore.com/"
+    
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
+        
+    def navigate(self,):
+    
+    
+        
